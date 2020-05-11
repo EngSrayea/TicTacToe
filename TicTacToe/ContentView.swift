6d28d5cd9dabab90c8705b7f751c0af817376f34
EngSrayea <eng.nsrayea@gmail.com>
2020-05-11 03:48:20 +0300

    Neat X-O for submission =) !

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,10 +8,92 @@
 
 import SwiftUI
 
+struct Field {
+    var text: String
+    var enabled: Bool
+    
+}
+
 struct ContentView: View {
+    
+    @State var fields: [[Field]] = .init(repeating: .init(repeating: Field(text: "", enabled: true), count: 3), count: 3)
+    
+    @State var crrntPlayer: String = "X"
+    @State var winner = ""
+    @State var counter = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack {
+            Image("tt3").resizable().frame(width:480, height: 1000).scaledToFill()
+            VStack(spacing: 10){
+                Text(winner).foregroundColor(.yellow).foregroundColor(.white).font(Font.custom("PWChalk", size: 60)).offset(y: -50)
+                
+                Text("\(crrntPlayer) Turn").foregroundColor(.white).font(Font.custom("PWChalk", size: 60)).offset(y: -50)
+                
+                ForEach(0 ..< 3){ r in
+                    HStack(spacing: 10){
+                        ForEach(0 ..< 3){ c in
+                            Button(action: {
+                                if self.fields[r][c].enabled{
+                                    self.fields[r][c].text = self.crrntPlayer
+                                self.crrntPlayer = self.crrntPlayer == "X" ? "O" : "X"
+                                    self.fields[r][c].enabled = false
+                                    self.counter += 1
+                                    self.checkWinners()
+                                    
+                                }
+                            }) {
+                                Text(self.fields[r][c].text).foregroundColor(.yellow).font(Font.custom("PWChalk", size: 70))
+                                    .frame(width: 100, height: 100).background(Color.white).opacity(0.4)
+                            }
+                        }
+                    }
+                }
+                
+                if winner != "" {
+                    Button(action: {
+                        self.restartGame()
+                    }) {
+                        Text("RESTART GAME?").foregroundColor(.white).font(Font.custom("PWChalk", size: 35)).offset(y: 50)
+                    }
+                }
+                
+            }
+        }
     }
+
+    func checkWinners() {
+        func checkWinner(plyr : String){
+    let r1 = fields[0][0].text == plyr && fields[0][1].text == plyr && fields[0][2].text == plyr
+    let r2 = fields[1][0].text == plyr && fields[1][1].text == plyr && fields[1][2].text == plyr
+    let r3 = fields[2][0].text == plyr && fields[2][1].text == plyr && fields[2][2].text == plyr
+    let c1 = fields[0][0].text == plyr && fields[1][0].text == plyr && fields[2][0].text == plyr
+    let c2 = fields[0][1].text == plyr && fields[1][1].text == plyr && fields[2][1].text == plyr
+    let c3 = fields[0][2].text == plyr && fields[1][2].text == plyr && fields[2][2].text == plyr
+    let d1 = fields[0][0].text == plyr && fields[1][1].text == plyr && fields[2][2].text == plyr
+    let d2 = fields[0][2].text == plyr && fields[1][1].text == plyr && fields[2][0].text == plyr
+    
+    if r1 || r2 || r3 || c1 || c2 || c3 || d1 || d2 {
+        winner = "\(plyr) wins!"
+            }
+            else if counter == 9 {
+            winner = "DRAW!"
+            }
+        }
+        checkWinner(plyr: "X")
+        checkWinner(plyr: "O")
+    }
+    
+    
+    
+    func restartGame(){
+        fields = .init(repeating: .init(repeating: Field(text: "", enabled: true), count: 3), count: 3)
+        crrntPlayer = "X"
+        winner = ""
+        counter = 0
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +101,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
